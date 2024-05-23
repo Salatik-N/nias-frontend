a34ba9c5039e8ba25b490231ed2687d9a6de5e51
Salatik-N <nikita-salata@mail.ru>
2024-05-23 12:49:35 +0200

    ADD: buttons color with main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import { getGlobalPageData } from '@/data/loaders'
+import Container from '@/components/custom/Container'
+import styles from './page.module.scss'
+import Link from 'next/link'
 
 export default async function Page() {
-  return <div>123</div>
+  return (
+    <Container>
+      <div className={styles.rowContentBlock}>
+        <div className={styles.rowContentBlockLeft}>
+          <span className={styles.superhead}>Hi all. I am</span>
+          <span className="h1">Name</span>
+          <p className="h2">Front-end developer</p>
+          <p>// you can also see it on my Github page</p>
+          <Link href="https://github.com/Salatik-N" target="_blank">
+            const githubLink = “https://github.com/Salatik-N”
+          </Link>
+          <Link href="/projects" className="button button-yellow">
+            Projects
+          </Link>
+        </div>
+        <div className={styles.rowContentBlockRight}></div>
+      </div>
+    </Container>
+  )
 }
